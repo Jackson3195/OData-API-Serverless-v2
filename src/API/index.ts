93520c63b72e98b695c1d6771aecba95bff304c1
Jackson Jacob <jjpro@Jacksons-MacBook-Pro.local>
2020-11-09 14:50:00 +0000

    - Added watch abillity so that hot reloading works

--- src/API/index.ts
+++ src/API/index.ts
@@ -2,9 +2,10 @@ import { Context, HttpRequest } from '@azure/functions';
 
 // TODO: Ensure alais imports work for example @interfaces/modules should then blah/blah/interfaces/modules
 module.exports = async function (ctx: Context, _req: HttpRequest) {
-    ctx.log.warn('Hello world');
+    const message = 'Hello World!!!!';
+    ctx.log.warn(message);
     ctx.res.status = 200;
     ctx.res.body = {
-        message: 'Hello world'
+        message,
     };
 };
